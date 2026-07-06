46110f71f331136f1b3ef381c28f52cd6859ebb1
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-06 13:06:52 +0545

    Update and rename terminal.h to terminal.hpp

---
+++ include/terminal.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <utility>
+
+namespace terminal {
+    int enable_raw_mode();
+    int disable_raw_mode();
+    void switch_to_alternate_screen_buffer();
+    void return_to_main_screen_buffer();
+    void clear_screen();
+    void move_cursor_to_home();
+    void hide_cursor();
+    void show_cursor();
+    void clear_line();
+    std::pair<int, int> get_window_size();
+    
+    // RAII wrapper for terminal mode
+    class TerminalGuard {
+    public:
+        TerminalGuard();
+        ~TerminalGuard();
+        TerminalGuard(const TerminalGuard&) = delete;
+        TerminalGuard& operator=(const TerminalGuard&) = delete;
+        TerminalGuard(TerminalGuard&&) = delete;
+        TerminalGuard& operator=(TerminalGuard&&) = delete;
+    };
+}
