adf16a62bcf30b761cb48e184faa4d41d21e63a8
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-06 13:23:24 +0545

    Update terminal.hpp

--- include/terminal.hpp
+++ include/terminal.hpp
@@ -17,6 +17,7 @@ namespace terminal {
     void clear_line();
     std::pair<int, int> get_window_size();
     
+    // RAII wrapper for terminal mode
     class TerminalGuard {
     public:
         TerminalGuard();
