97ae5bacbd1476836ae25fc8f1b249d5fcb6df09
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-06 13:09:06 +0545

    Update cli.hpp

--- include/cli.hpp
+++ include/cli.hpp
@@ -1,22 +1,78 @@
-#pragma once
-
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <string>
-#include <vector>
+#include "cli.hpp"
 #include "config.hpp"
 
 namespace cli {
-    struct Options {
-        bool show_help = false;
-        bool show_version = false;
-        std::string filename;
-    };
-    
-    void show_no_input();
-    void show_help();
-    void show_version();
-    void show_unknown(const std::string& arg);
-    Options parse_arguments(int argc, char* argv[]);
+    void show_no_input() {
+        fprintf(stderr, "%s: no input file provided\n", config::TARGET);
+        fprintf(stderr, "Try %s'%s --help'%s for more information.\n", 
+                config::color::GREEN, config::TARGET, config::color::RESET);
+    }
+
+    void show_help() {
+        printf("%s %s - %s\n\n", 
+               config::color::BOLD, config::TARGET, config::DESCRIPTION);
+        printf("%s", config::color::RESET);
+        printf("Usage: %s [OPTIONS] [FILE]\n\n", config::TARGET);
+        
+        printf("Options:\n");
+        printf("  %s-h%s, %s--help%s      Show this help message\n", 
+               config::color::YELLOW, config::color::RESET, 
+               config::color::YELLOW, config::color::RESET);
+        printf("  %s-v%s, %s--version%s   Show version information\n", 
+               config::color::YELLOW, config::color::RESET,
+               config::color::YELLOW, config::color::RESET);
+        
+        printf("\nKeybindings:\n");
+        printf("  %sCtrl-Q%s              Quit editor\n", 
+               config::color::YELLOW, config::color::RESET);
+        printf("  %sCtrl-S%s              Save file\n", 
+               config::color::YELLOW, config::color::RESET);
+        printf("  %sCtrl-O%s              Open file\n", 
+               config::color::YELLOW, config::color::RESET);
+        printf("  %sArrow Keys%s          Navigate\n", 
+               config::color::YELLOW, config::color::RESET);
+        printf("  %sTab%s                 Insert tab\n", 
+               config::color::YELLOW, config::color::RESET);
+    }
+
+    void show_version() {
+        printf("%s %s\n", config::TARGET, config::VERSION);
+    }
+
+    void show_unknown(const std::string& arg) {
+        fprintf(stderr, "%s: unknown option '%s'\n", config::TARGET, arg.c_str());
+        fprintf(stderr, "Try %s'%s --help'%s for more information.\n", 
+                config::color::GREEN, config::TARGET, config::color::RESET);
+    }
+
+    Options parse_arguments(int argc, char* argv[]) {
+        Options opts;
+        
+        for (int i = 1; i < argc; i++) {
+            std::string arg = argv[i];
+            
+            if (arg == "-h" || arg == "--help") {
+                opts.show_help = true;
+                return opts;
+            } else if (arg == "-v" || arg == "--version") {
+                opts.show_version = true;
+                return opts;
+            } else if (arg[0] == '-') {
+                show_unknown(arg);
+                exit(1);
+            } else if (opts.filename.empty()) {
+                opts.filename = arg;
+            } else {
+                fprintf(stderr, "%s: too many arguments\n", config::TARGET);
+                exit(1);
+            }
+        }
+        
+        if (opts.filename.empty()) {
+            show_no_input();
+            exit(1);
+        }
+        
+        return opts;
+    }
 }
